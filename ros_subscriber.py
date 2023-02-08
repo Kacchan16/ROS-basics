0a33037e8fe5eb0d95a5b7a3e4f4521eb81d5559
Kacchan <dekusmash016@gmail.com>
2023-02-08 22:31:33 +0100

    listener is done

--- ros_subscriber.py
+++ ros_subscriber.py
@@ -1,1 +1,15 @@
-import from
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+def callback_function(message):
+   rospy.loginfo(rospy.get_caller_id() + "I heard %s", message.data)
+
+def listener():
+   rospy.init_node('listener', anonymous=True)
+   rospy.subscriber('chatter',String, callback_function)
+   ros.spin()
+   
+if __name__ == '__main__'
+    listener()
